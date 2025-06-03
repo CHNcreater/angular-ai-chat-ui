3128a851bece6bdf699164641bbd54fb07d6eccc
qiaoyinbo <breakerjoe@163.com>
2025-06-03 21:48:34 +0800

    update ng and angular material and add button to side bar

--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss',
 })
